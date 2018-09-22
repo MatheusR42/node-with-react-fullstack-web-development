41a16e9bdca3e94d43237d99607225f7f86eff72
MatheusR42 <matheus.araujo.r42@gmail.com>
2018-09-22 16:16:30 -0300

    68. Route Configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <div>Header</div>;
+const DashBoard = () => <div>DashBoard</div>;
+const SurveyNew = () => <div>SurveyNew</div>;
+const Landing = () => <div>Landing</div>;
 
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={Landing} />
+                    <Route path="/surveys" exact component={DashBoard} />
+                    <Route path="/surveys/new" exact component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
