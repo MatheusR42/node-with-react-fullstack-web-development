cfcd8e69bbce8254c31226bf6492523597462c1f
Matheus Araujo <matheus.araujo.r42@gmail.com>
2018-09-23 16:02:05 -0300

    92. Env Variables with React

--- client/src/index.js
+++ client/src/index.js
@@ -14,3 +14,6 @@ ReactDOM.render(
     </Provider>,
     document.querySelector('#root')
 );
+
+console.log('stripe key: ', process.env.REACT_APP_STRIPE_KEY);
+console.log('stripe key: ', process.env.NODE_ENV);
