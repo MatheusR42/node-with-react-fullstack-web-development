1e8472ac6d26afb64414cf9517fcd988b2441c34
Matheus Araujo <matheus.araujo.r42@gmail.com>
2018-09-23 18:01:51 -0300

    103. Adding Credits to a User

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 require('./models/User');
 require('./services/passport');
 const express = require("express");
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+const billingRoutes = require('./routes/billingRoutes');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+billingRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,18 @@
+const keys = require('../config/keys');
+const stripe = require("stripe")(keys.stripeSecretKey);
+
+module.exports = app => {
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id //id created on front-end by react-stripe-checkout
+        });
+
+        req.user.credits += 5; //using passport we always have acess to user
+        const user = await req.user.save(); //save in database
+        
+        res.send(user);
+    });
+}
